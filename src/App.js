0754441b89fc5c1c78e17d64de901c0885dbf063
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-07 23:57:00 +0700

    d

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Carousel from "./Carousel/Carousel"
 import Hero from "./component/Hero"
 import About from "./component/About"
 import './App.css';
+import {Helmet} from "react-helmet";
 
 
 const Test = styled.div`
@@ -20,6 +21,9 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
+        <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        </Helmet>
         <Header/>
         <Hero/>
         <p className = "title">My Project</p>
