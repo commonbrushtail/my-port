722949411f8620e5d49904225d6cf1dd27babff9
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-07 23:57:25 +0700

    Updates

--- precache-manifest.deca7b44968b1051b2db6040b5859d9d.js
+++ precache-manifest.deca7b44968b1051b2db6040b5859d9d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68b84a636c8b6243545b70d947aee41a",
+    "revision": "541200fc48a77a3d3d6243010b5c4c84",
     "url": "/my-port/index.html"
   },
   {
-    "revision": "c3f407276158fd9057f5",
+    "revision": "cc2e0fd78b98e16eef21",
     "url": "/my-port/static/css/main.a5fb455b.chunk.css"
   },
   {
-    "revision": "60d0b82a0d936c8e7914",
-    "url": "/my-port/static/js/2.b64a9806.chunk.js"
+    "revision": "c65778b7ff1d4abb8182",
+    "url": "/my-port/static/js/2.e3e70437.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/my-port/static/js/2.b64a9806.chunk.js.LICENSE.txt"
+    "url": "/my-port/static/js/2.e3e70437.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c3f407276158fd9057f5",
-    "url": "/my-port/static/js/main.0a88c88b.chunk.js"
+    "revision": "cc2e0fd78b98e16eef21",
+    "url": "/my-port/static/js/main.cfa749e6.chunk.js"
   },
   {
     "revision": "647724e9d0a47660c904",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-port/precache-manifest.0dbab59155e698a914e523b92db9863f.js"
+  "/my-port/precache-manifest.deca7b44968b1051b2db6040b5859d9d.js"
 );
 
 self.addEventListener('message', (event) => {
