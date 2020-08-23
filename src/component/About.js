7100ba8a7613845b4c199af597c1e1a508e1f236
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-23 20:36:21 +0700

    1

--- src/component/About.js
+++ src/component/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="aboutContainer">
             <div className ="aboutWrap">
-                <img src="front.jpg"/>
+                <img class="aboutMeImg" src="front.jpg"/>
                 <p>About Me</p>
                 <span>As a footwear production manager, I help reduce manufacturing costs by millions while corroborating with my team and care for their well being. After spending my free time learning web development for months, I decided to quit my job to dedicate myself to becoming a web developer
                 
